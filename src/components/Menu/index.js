f56b160f8d2feea90f07995bbc34125105bc81e9
Julia Soto <julia_lethicia.s.a@hotmail.com>
2020-08-18 02:00:43 -0300

    atualizando Button

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
-import ButtonLink from '../ButtonLink';
+import Button from '../Button'
+//import ButtonLink from './components/ButtonLink';
 
 function Menu(){
     return (
@@ -9,9 +10,9 @@ function Menu(){
             <a href="/">
         <img className="Logo" src={Logo} alt="JuFlix logo"/>
             </a>
-            <ButtonLink className="ButtonLink" href="/">
+            <Button as="a" className="ButtonLink" href="/">
                 Novo vídeo
-            </ButtonLink>
+            </Button>
             
         </nav>
     )
